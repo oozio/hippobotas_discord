d4f1c806c4b9cc015a5cbee7b6e0f56764ed5ba5
oozio <xxue@caltech.edu>
2021-08-22 23:15:30 -0700

    Update handler.py

--- mal/handler.py
+++ mal/handler.py
@@ -7,6 +7,6 @@ def handle(command, input):
     if command == 'addmal':
         return f"haven't implemented {command} yet, check back later :3"
     if command == 'mal': 
-        return mal.get_mal_user(input.get('MAL_name'))
+        return mal.get_mal_user(input.get('mal_name'))
     
     return f"UNKNOWN COMMAND: {command}"
