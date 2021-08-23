8d04c56fd72d8615c88d2ce2c74103b3cdcb6dac
oozio <xxue@caltech.edu>
2021-08-22 21:19:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import json
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
-PUBLIC_KEY = '7f28e2f1adb556db43a2e4d3bcde1ea177312d7a82c0e72fad5d005a1284f889'
-PING_PONG = {"type": 1}
 RESPONSE_TYPES =  { 
                     "PONG": 1, 
                     "ACK_NO_SOURCE": 2, 
@@ -13,36 +11,8 @@ RESPONSE_TYPES =  {
                     "ACK_WITH_SOURCE": 5
                   }
 
-
-def verify_signature(event):
-    raw_body = event.get("rawBody")
-    auth_sig = event['params']['header'].get('x-signature-ed25519')
-    auth_ts  = event['params']['header'].get('x-signature-timestamp')
-    
-    message = auth_ts.encode() + raw_body.encode()
-    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
-    verify_key.verify(message, bytes.fromhex(auth_sig)) # raises an error if unequal
-
-def ping_pong(body):
-    if body.get("type") == 1:
-        return True
-    return False
     
 def lambda_handler(event, context):
-    print(f"event {event}") # debug print
-    # verify the signature
-    try:
-        verify_signature(event)
-    except Exception as e:
-        raise Exception(f"[UNAUTHORIZED] Invalid request signature: {e}")
-
-
-    # check if message is a ping
-    body = event.get('body-json')
-    if ping_pong(body):
-        return PING_PONG
-    
-    
     # dummy return
     return {
             "type": RESPONSE_TYPES['MESSAGE_NO_SOURCE'],
